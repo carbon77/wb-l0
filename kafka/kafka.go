76341148f0f9b655e192670a41ad2791a9617f02
Igor <zakatov0303@gmail.com>
2024-11-19 21:25:46 +0300

    Add kafka producer and endpoint for sending order

--- kafka/kafka.go
+++ kafka/kafka.go
@@ -9,11 +9,17 @@ import (
 	"ru/zakat/L0/models"
 
 	"github.com/IBM/sarama"
+	"go.uber.org/zap"
 )
 
 var (
-	log = logger.NewLogger()
+	log     = logger.NewLogger()
+	address = []string{"localhost:9094"}
 )
+
+type OrderProducer struct {
+	producer sarama.SyncProducer
+}
 
 type consumerGroupHandler struct {
 	cache *cache.Cache
@@ -23,14 +29,14 @@ func (consumerGroupHandler) Setup(_ sarama.ConsumerGroupSession) error   { retur
 func (consumerGroupHandler) Cleanup(_ sarama.ConsumerGroupSession) error { return nil }
 func (h consumerGroupHandler) ConsumeClaim(sess sarama.ConsumerGroupSession, claim sarama.ConsumerGroupClaim) error {
 	for msg := range claim.Messages() {
-		log.Info("Received message from \"orders\" topic")
+		log.Info("Received message", zap.String("topic", "orders"))
 		var order models.Order
 		err := json.Unmarshal(msg.Value, &order)
 
 		if err != nil {
-			log.Error("Can't read order from message")
+			log.Error("Can't read order from message", zap.Error(err))
 		} else {
-			log.Info("Order has been read")
+			log.Info("Order has been read", zap.String("order_uid", order.UID))
 			h.cache.AddOrder(&order)
 		}
 
@@ -40,7 +46,7 @@ func (h consumerGroupHandler) ConsumeClaim(sess sarama.ConsumerGroupSession, cla
 }
 
 func NewConsumer(cache *cache.Cache) {
-	group, err := sarama.NewConsumerGroup([]string{"localhost:9094"}, "my-group", nil)
+	group, err := sarama.NewConsumerGroup(address, "my-group", nil)
 	if err != nil {
 		panic(err)
 	}
@@ -61,5 +67,36 @@ func NewConsumer(cache *cache.Cache) {
 		if err != nil {
 			panic(err)
 		}
+	}
+}
+
+func NewProducer() *OrderProducer {
+	producer, err := sarama.NewSyncProducer(address, nil)
+	if err != nil {
+		log.Error("Can't create sync producer", zap.Error(err))
+	}
+
+	return &OrderProducer{
+		producer: producer,
+	}
+}
+
+func (p *OrderProducer) SendOrder(topic string, order *models.Order) {
+	bytes, err := json.Marshal(order)
+	if err != nil {
+		log.Error("Can't marshall order", zap.Error(err))
+	}
+
+	msg := &sarama.ProducerMessage{
+		Topic: topic,
+		Key:   sarama.StringEncoder(order.UID),
+		Value: sarama.ByteEncoder(bytes),
+	}
+
+	_, _, err = p.producer.SendMessage(msg)
+	if err != nil {
+		log.Error("Failed to send message", zap.Error(err))
+	} else {
+		log.Info("Order sent", zap.String("order_uid", order.UID))
 	}
 }
--- main.go
+++ main.go
@@ -7,18 +7,34 @@ import (
 	"ru/zakat/L0/db"
 	"ru/zakat/L0/kafka"
 	"ru/zakat/L0/logger"
+	"ru/zakat/L0/models"
 
 	"github.com/gin-gonic/gin"
+	"go.uber.org/zap"
 )
 
 func main() {
-	logger := logger.NewLogger()
+	log := logger.NewLogger()
 	repo := db.NewRepository()
 	cache := cache.NewCache(repo)
-	go kafka.NewConsumer(cache)
+	orderProducer := kafka.NewProducer()
 	r := gin.Default()
 
-	r.GET("/orders/send", func(c *gin.Context) {
+	go kafka.NewConsumer(cache)
+
+	r.POST("/orders/send", func(c *gin.Context) {
+		var order models.Order
+
+		if err := c.BindJSON(&order); err != nil {
+			log.Error("Failed to read request body", zap.Error(err))
+			c.JSON(http.StatusBadRequest, gin.H{
+				"message": "Failed to read request body",
+			})
+			return
+		}
+
+		orderProducer.SendOrder("orders", &order)
+		c.JSON(http.StatusOK, gin.H{})
 	})
 
 	r.GET("/orders", func(c *gin.Context) {
@@ -40,6 +56,6 @@ func main() {
 		c.JSON(http.StatusOK, order)
 	})
 
-	logger.Info("Starting server...")
+	log.Info("Starting server...")
 	r.Run()
 }
